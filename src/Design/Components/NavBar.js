63b241fc09777cd1d6fbf552cd9bd92a476164ac
Darshana Perera <ds.perera.1997@gmail.com>
2024-07-28 22:25:14 +0530

    navbar basic created

---
+++ src/Design/Components/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="Home" />
+      </div>
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="Chat" />
+      </div>
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="Stats" />
+      </div>
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="Settings" />
+      </div>
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="Info" />
+      </div>
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="User" />
+      </div>
+      <div className="navbar-item">
+        <img src="https://via.placeholder.com/40" alt="More" />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
